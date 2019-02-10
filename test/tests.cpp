dfde5081f56362bb61ce2a37b12a54a06ba61bf4
robertDurst <rsdurst@colby.edu>
2019-02-10 15:19:43 -0500

    making progress on tests, helloworld passes, fizzbuzz does not

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,10 +6,10 @@
 #include "../src/sailfish/Lexar.h"
 #include <gtest/gtest.h>
 
-TEST(LexarTest, HelloWorld.fish)
+TEST(LexarTest, HelloWorld)
 {
-    static const Kind expected[] = {Kind::COMMENT_TOKEN, Kind::KEYWORD_TOKEN,
-                                    Kind::LCURLEY_TOKEN, Kind::KEYWORD_TOKEN,
+    static const Kind expected[] = {Kind::COMMENT_TOKEN, Kind::IDENTIFIER_TOKEN,
+                                    Kind::LCURLEY_TOKEN, Kind::IDENTIFIER_TOKEN,
                                     Kind::LPAREN_TOKEN,  Kind::STRING_TOKEN,
                                     Kind::RPAREN_TOKEN,  Kind::RCURLEY_TOKEN};
 
@@ -22,7 +22,52 @@ TEST(LexarTest, HelloWorld.fish)
 
     while (!t->isEOF())
     {
-        EXPECT_EQ(expected[i], t->getKind());
+        ASSERT_EQ(expected[i], t->getKind());
+        t = lexar->getToken();
+        ++i;
+    }
+}
+
+TEST(LexarTest, FizzBuzz)
+{
+    static const Kind expected[] = {
+        Kind::KEYWORD_TOKEN,    Kind::IDENTIFIER_TOKEN, Kind::ARROW_TOKEN,
+        Kind::IDENTIFIER_TOKEN, Kind::IDENTIFIER_TOKEN, Kind::ARROW_TOKEN,
+        Kind::IDENTIFIER_TOKEN, Kind::LCURLEY_TOKEN,    Kind::KEYWORD_TOKEN,
+        Kind::LPAREN_TOKEN,     Kind::IDENTIFIER_TOKEN, Kind::OPERATION_TOKEN,
+        Kind::LOGIC_TOKEN,      Kind::EOF_TOKEN,        Kind::RPAREN_TOKEN,
+        Kind::LCURLEY_TOKEN,    Kind::IDENTIFIER_TOKEN, Kind::LPAREN_TOKEN,
+        Kind::STRING_TOKEN,     Kind::RPAREN_TOKEN,     Kind::RCURLEY_TOKEN,
+        Kind::KEYWORD_TOKEN,    Kind::LPAREN_TOKEN,     Kind::IDENTIFIER_TOKEN,
+        Kind::OPERATION_TOKEN,  Kind::INTEGER_TOKEN,    Kind::LOGIC_TOKEN,
+        Kind::INTEGER_TOKEN,    Kind::RPAREN_TOKEN,     Kind::LCURLEY_TOKEN,
+        Kind::IDENTIFIER_TOKEN, Kind::LPAREN_TOKEN,     Kind::STRING_TOKEN,
+        Kind::RPAREN_TOKEN,     Kind::RCURLEY_TOKEN,    Kind::KEYWORD_TOKEN,
+        Kind::LPAREN_TOKEN,     Kind::IDENTIFIER_TOKEN, Kind::OPERATION_TOKEN,
+        Kind::INTEGER_TOKEN,    Kind::LOGIC_TOKEN,      Kind::INTEGER_TOKEN,
+        Kind::RPAREN_TOKEN,     Kind::LCURLEY_TOKEN,    Kind::IDENTIFIER_TOKEN,
+        Kind::LPAREN_TOKEN,     Kind::STRING_TOKEN,     Kind::RPAREN_TOKEN,
+        Kind::RCURLEY_TOKEN,    Kind::KEYWORD_TOKEN,    Kind::LCURLEY_TOKEN,
+        Kind::IDENTIFIER_TOKEN, Kind::LPAREN_TOKEN,     Kind::IDENTIFIER_TOKEN,
+        Kind::RPAREN_TOKEN,     Kind::RCURLEY_TOKEN,    Kind::RCURLEY_TOKEN,
+        Kind::IDENTIFIER_TOKEN, Kind::LCURLEY_TOKEN,    Kind::COMMENT_TOKEN,
+        Kind::KEYWORD_TOKEN,    Kind::IDENTIFIER_TOKEN, Kind::OPERATION_TOKEN,
+        Kind::LBRACKET_TOKEN,   Kind::INTEGER_TOKEN,    Kind::COMMA_TOKEN,
+        Kind::INTEGER_TOKEN,    Kind::RBRACKET_TOKEN,   Kind::LCURLEY_TOKEN,
+        Kind::IDENTIFIER_TOKEN, Kind::LPAREN_TOKEN,     Kind::IDENTIFIER_TOKEN,
+        Kind::RPAREN_TOKEN,     Kind::RCURLEY_TOKEN,    Kind::RCURLEY_TOKEN,
+    };
+
+    Lexar* lexar = new Lexar("../sailfish_examples/fizzbuzz.fish");
+
+    Token* t;
+    int i = 0;
+
+    t = lexar->getToken();
+
+    while (!t->isEOF())
+    {
+        ASSERT_EQ(expected[i], t->getKind());
         t = lexar->getToken();
         ++i;
     }
